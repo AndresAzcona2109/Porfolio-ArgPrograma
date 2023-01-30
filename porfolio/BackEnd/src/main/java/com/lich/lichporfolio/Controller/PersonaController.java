fab25b5d2344f75bb3b519810a1f307ef2c05b53
AndresAzcona2109 <lich.ghoul1@gmail.com>
2023-01-30 18:23:40 -0300

    login backend

--- porfolio/BackEnd/src/main/java/com/lich/lichporfolio/Controller/PersonaController.java
+++ porfolio/BackEnd/src/main/java/com/lich/lichporfolio/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.lich.lichporfolio.Entity.Persona;
 import com.lich.lichporfolio.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,44 +20,45 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
-    
-    @GetMapping("/personas/traer")
+
+    @GetMapping("personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
-    
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
-        return "La persona fue creada";
+        return "La persona fue creada correctamente";
     }
-    
+
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
-        return "La persona fue borrada";
+        return "La persona fue eliminada correctamente";
     }
-    
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam("nombre") String nuevoNombre,
                                @RequestParam("apellido") String nuevoApellido,
                                @RequestParam("img") String nuevoImg){
         Persona persona = ipersonaService.findPersona(id);
-        
+
         persona.setNombre(nuevoNombre);
         persona.setApellido(nuevoApellido);
         persona.setImg(nuevoImg);
-        
+
         ipersonaService.savePersona(persona);
         return persona;
-             
     }
-    
-    @GetMapping("/personas/traer/perfil")
+
+    @GetMapping("personas/traer/perfil")
     public Persona findPersona(){
-        return ipersonaService.findPersona((long)2);
+        return ipersonaService.findPersona((long)1);
     }
 
 }
---
+++ porfolio/BackEnd/src/main/java/com/lich/lichporfolio/Security/Service/UserDetailsImpl.java
@@ -0,0 +1,29 @@
+
+
+package com.lich.lichporfolio.Security.Service;
+
+/**
+ *
+ * @author Lich
+ */
+import com.lich.lichporfolio.Security.Entity.Usuario;
+import com.lich.lichporfolio.Security.Entity.UsuarioPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsImpl implements UserDetailsService{
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
+        return UsuarioPrincipal.build(usuario);
+    }
+
+
+}
